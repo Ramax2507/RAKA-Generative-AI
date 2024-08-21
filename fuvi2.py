58560a54b328947c142acbc790607b3654831e11
Ramax2507 <147583858+Ramax2507@users.noreply.github.com>
2024-08-21 20:40:34 +0530

    Update fuvi2.py

--- fuvi2.py
+++ fuvi2.py
@@ -1,19 +1,17 @@
 import streamlit as st
 import google.generativeai as genai
 
-st.title("Welcome to the App")
 
-genai.configure(api_key="AIzaSyBxbej54VRifGyHUARbSe8dSxJ1XK8jS0s")
+st.title("welcome to RAKA Gen AI")
 
+genai.configure(api_key="AIzaSyBxbej54VRifGyHUARbSe8dSxJ1XK8jS0s")  
 
 text = st.text_input("Hey!! How do you want me to help you today?")
 
-if text:
- 
-    model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat(history=[])
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
 
-    if st.button("Enter"):
 
-         response = chat.send_message(text)
-         st.write(response['text']) 
+if st.button("Click me"):
+    response = chat.send_message(text)
+    st.write(response.text)
